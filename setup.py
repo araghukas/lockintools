5ed6201c729dc5c45175b312cd810337de44de1b
Ara Ghukasyan <ghukasa@mcmaster.ca>
2019-11-26 13:01:19 -0500

    added requirement 'openpyxl'

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ setup(
     name="lockintools",
     version=__version__,
     description="Interface tools for Stanford Research Instruments lock-in amplifier "
-                 "model SR830 DSP; used for 3ω thermal conductivity measurements.",
+    "model SR830 DSP; used for 3ω thermal conductivity measurements.",
     author="Pedro Oliviera & Ara Ghukasyan",
     author_email="ghukasa@mcmaster.ca",
     py_modules=["tools", "settings"],
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "simpleaudio", "pyserial", "datetime"],
+    install_requires=["numpy",
+                      "pandas",
+                      "simpleaudio",
+                      "pyserial",
+                      "datetime",
+                      "openpyxl"],
     zip_safe=False
 )
