176a5d3ac660c030ed974d5e223b81cc2feff9b8
araghukas <ghukasa@mcmaster.ca>
2020-09-14 13:59:45 -0400

    rename data to lockindata

--- lockintools/measure.py
+++ lockintools/measure.py
@@ -31,7 +31,7 @@ class Measure3w(object):
         self.ampls = ampls
         self.lock = lock
         self.label = label
-        self.data = LockInData(working_dir, create_dir)
+        self.lockindata = LockInData(working_dir, create_dir)
 
     @property
     def label(self):
@@ -81,25 +81,26 @@ class Measure3w(object):
             raise ValueError("must assign `LockIn` object to this attribute")
 
     def sweep_sample1w(self, **kwargs):
-        # TODO: which values are used if kwargs is not {}
         _kwargs = Measure3w.sample_1w_defaults.copy()
         for key, value in kwargs.items():
             _kwargs[key] = value
+
         print("sweeping sample 1-omega voltage")
         sweep_data = self.lock.sweep(label=self.label, freqs=self.freqs,
                                      ampls=self.ampls, harm=1, **_kwargs)
-        self.data.add_sweeps(Vs_1w=sweep_data)
-        self.data.save_all()
+        self.lockindata.add_sweeps(Vs_1w=sweep_data)
+        self.lockindata.save_all()
 
     def sweep_sample3w(self, **kwargs):
         _kwargs = Measure3w.sample_3w_defaults.copy()
         for key, value in kwargs.items():
             _kwargs[key] = value
+
         print("sweeping sample 3-omega voltage")
         sweep_data = self.lock.sweep(label=self.label, freqs=self.freqs,
                                      ampls=self.ampls, harm=3, **_kwargs)
-        self.data.add_sweeps(Vs_3w=sweep_data)
-        self.data.save_all()
+        self.lockindata.add_sweeps(Vs_3w=sweep_data)
+        self.lockindata.save_all()
 
     def sweep_shunt1w(self, countdown=True, count=30, **kwargs):
         _kwargs = Measure3w.shunt_1w_defaults.copy()
@@ -118,8 +119,8 @@ class Measure3w(object):
         print("sweeping shunt 1-omega voltage")
         sweep_data = self.lock.sweep(label=self.label, freqs=self.freqs,
                                      ampls=self.ampls, harm=1, **_kwargs)
-        self.data.add_sweeps(Vsh_1w=sweep_data)
-        self.data.save_all()
+        self.lockindata.add_sweeps(Vsh_1w=sweep_data)
+        self.lockindata.save_all()
 
     def test_run(self):
         self.sweep_sample1w()
@@ -129,7 +130,7 @@ class Measure3w(object):
 
     def save_tc3omega(self):
         if np.isscalar(self.ampls):
-            self.data.save_tc3omega(self.ampls)
+            self.lockindata.save_tc3omega(self.ampls)
         else:
             for ampl in self.ampls:
-                self.data.save_tc3omega(ampl)
+                self.lockindata.save_tc3omega(ampl)
